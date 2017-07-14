1bd58cade0f14ba49c4c3405a7cce0a2a2b8a6b8
jeffreymor <jeffreymor@qq.com>
2017-07-14 15:58:09 +0800

    Some fixes

--- app/src/main/java/com/jeffreymor/googlehosts/SettingsActivity.java
+++ app/src/main/java/com/jeffreymor/googlehosts/SettingsActivity.java
@@ -8,7 +8,7 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_setting);
+        setContentView(R.layout.activity_settings);
         getFragmentManager().beginTransaction()
                 .replace(R.id.settings_container, new SettingsFragment())
                 .commit();
